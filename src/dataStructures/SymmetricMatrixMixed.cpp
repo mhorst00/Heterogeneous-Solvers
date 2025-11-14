13abeb7eba33b87a99940336d3e4ff1008baf6bf
maltehorst <maltehorst@gmail.com>
2025-11-14 17:11:32 +0100

    started work on mixed precision matrix generator

---
+++ src/dataStructures/SymmetricMatrixMixed.cpp
@@ -0,0 +1,22 @@
+#include <hws/system_hardware_sampler.hpp>
+#include <sycl/sycl.hpp>
+#include <vector>
+
+#include "SymmetricMatrixMixed.hpp"
+
+#include "Configuration.hpp"
+
+SymmetricMatrixMixed::SymmetricMatrixMixed(
+    const std::size_t N, const std::vector<Precision> precisionVector,
+    const int blockSize, sycl::queue &queue)
+    : N(N), blockSize(blockSize),
+      blockCountXY(
+          std::ceil(static_cast<double>(N) / static_cast<double>(blockSize))),
+      precisionVector(precisionVector),
+      matrixData(
+          sycl::usm_allocator<conf::fp_type, sycl::usm::alloc::host>(queue)) {
+  // allocate memory for matrix storage
+  const std::size_t blockCount = (blockCountXY * (blockCountXY + 1)) / 2.0;
+  matrixData.resize(blockCount *
+                    static_cast<std::size_t>(blockSize * blockSize));
+}
--- src/utility/MatrixGenerator.hpp
+++ src/utility/MatrixGenerator.hpp
@@ -5,61 +5,91 @@
 
 #include "RightHandSide.hpp"
 #include "SymmetricMatrix.hpp"
+#include "SymmetricMatrixMixed.hpp"
 
 /**
- * Class that contains various methods for generating symmetric positive-definite (SPD) matrices
+ * Class that contains various methods for generating symmetric
+ * positive-definite (SPD) matrices
  */
 class MatrixGenerator {
 public:
-    /**
-     * This operation generates a diagonal dominant SPD matrix. (Currently not used)
-     *
-     * @param queue SYCL queue
-     * @return a SPD matrix
-     */
-    static SymmetricMatrix generateSPDMatrixStrictDiagonalDominant(sycl::queue& queue);
+  /**
+   * This operation generates a diagonal dominant SPD matrix. (Currently not
+   * used)
+   *
+   * @param queue SYCL queue
+   * @return a SPD matrix
+   */
+  static SymmetricMatrix
+  generateSPDMatrixStrictDiagonalDominant(sycl::queue &queue);
 
-    /**
-     * This operation generates a SPD kernel matrix that can be used for Gaussian Processes
-     *
-     * @param path path to text file with data
-     * @param queue CPU queue
-     * @param queueGPU GPU queue
-     * @return a SPD matrix
-     */
-    static SymmetricMatrix generateSPDMatrix(std::string& path, sycl::queue& queue, sycl::queue& queueGPU);
+  /**
+   * This operation generates a SPD kernel matrix that can be used for Gaussian
+   * Processes
+   *
+   * @param path path to text file with data
+   * @param queue CPU queue
+   * @param queueGPU GPU queue
+   * @return a SPD matrix
+   */
+  static SymmetricMatrix generateSPDMatrix(std::string &path,
+                                           sycl::queue &queue,
+                                           sycl::queue &queueGPU);
 
-    /**
-     * This operation generates the test kernel matrix K* used required for prediction with Gaussian Processes.
-     *
-     * @param path_train path to training data
-     * @param path_test path to test data
-     * @param queue CPU queue
-     * @param queueGPU GPU queue
-     * @param K_star the test kernel matrix
-     */
-    static void generateTestKernelMatrix(std::string& path_train, std::string& path_test, sycl::queue& queue, sycl::queue& queueGPU, conf::fp_type* K_star);
+  /**
+   * This operation generates a mixed precision SPD kernel matrix that can be
+   * used for Gaussian Processes
+   *
+   * @param path path to text file with data
+   * @param queue CPU queue
+   * @param queueGPU GPU queue
+   * @return a SPD matrix
+   */
+  static SymmetricMatrixMixed generateSPDMatrixMixed(std::string &path,
+                                                     sycl::queue &queue,
+                                                     sycl::queue &queueGPU);
 
-    /**
-     * This operation parses the right hand side based on data for Gaussian processses
-     *
-     * @param path path to right hand side data
-     * @param queue SYCL queue
-     * @return the right hand side
-     */
-    static RightHandSide parseRHS_GP(std::string& path, sycl::queue& queue);
+  /**
+   * This operation generates the test kernel matrix K* used required for
+   * prediction with Gaussian Processes.
+   *
+   * @param path_train path to training data
+   * @param path_test path to test data
+   * @param queue CPU queue
+   * @param queueGPU GPU queue
+   * @param K_star the test kernel matrix
+   */
+  static void generateTestKernelMatrix(std::string &path_train,
+                                       std::string &path_test,
+                                       sycl::queue &queue,
+                                       sycl::queue &queueGPU,
+                                       conf::fp_type *K_star);
 
-    /**
-     * This operation generates a right hand side randomly
-     *
-     * @param queue SYCL queue
-     * @return the right hand side
-     */
-    static RightHandSide generateRHS(sycl::queue& queue);
+  /**
+   * This operation parses the right hand side based on data for Gaussian
+   * processses
+   *
+   * @param path path to right hand side data
+   * @param queue SYCL queue
+   * @return the right hand side
+   */
+  static RightHandSide parseRHS_GP(std::string &path, sycl::queue &queue);
+
+  /**
+   * This operation generates a right hand side randomly
+   *
+   * @param queue SYCL queue
+   * @return the right hand side
+   */
+  static RightHandSide generateRHS(sycl::queue &queue);
 
 private:
-    static void readInputVector(std::string& path,std::vector<conf::fp_type, sycl::usm_allocator<conf::fp_type, sycl::usm::alloc::host>>& dataVector, int N, int offset);
+  static void readInputVector(
+      std::string &path,
+      std::vector<conf::fp_type,
+                  sycl::usm_allocator<conf::fp_type, sycl::usm::alloc::host>>
+          &dataVector,
+      int N, int offset);
 };
 
-
-#endif //MATRIXGENERATOR_HPP
+#endif // MATRIXGENERATOR_HPP
